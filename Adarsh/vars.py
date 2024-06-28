babd1b66f25c4398718e49b75e0f97d4504fdbda
Phoenix-0101 <patilharsh2629@gmail.com>
2024-06-28 20:02:36 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -36,5 +36,5 @@ class Var(object):
     else:
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://patilharsh_2629:<Harshpatil010>@storesbot.lkfapc9.mongodb.net/?retryWrites=true&w=majority&appName=Storesbot'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', '-1002174904158'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Gallery_Shop_Free'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
